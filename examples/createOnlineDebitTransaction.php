5a6e4400f7e8c2506a5fb4fad94ad99ff2f17fcb
Hugo Carreira <hugo.carreira@mundipagg.com>
2017-06-16 16:54:59 -0300

    example

---
+++ examples/createOnlineDebitTransaction.php
@@ -0,0 +1,63 @@
+<?php
+
+try
+{
+    // Load Dependencies
+    require_once('vendor/autoload.php');
+
+    // Define the used URL
+    \Gateway\ApiClient::setBaseUrl("https://sandbox.mundipaggone.com");
+
+    // Define the Merchant Key
+    \Gateway\ApiClient::setMerchantKey("8A2DD57F-1ED9-4153-B4CE-69683EFADAD5");
+
+    // Create request object
+    $request = new \Gateway\One\DataContract\Request\CreateSaleRequest();
+
+    // Online debit transacton data
+    $onlineDebitTransaction = new \Gateway\One\DataContract\Request\CreateSaleRequestData\OnlineDebitTransaction();
+    $request->addOnlineDebitTransaction($onlineDebitTransaction);
+
+    $onlineDebitTransaction
+        ->setAmountInCents(100)
+        ->setBank("Itau");
+
+    $request->getBuyer()
+        ->setName("Luke")
+        ->addAddress()
+        ->setAddressType(\Gateway\One\DataContract\Enum\AddressTypeEnum::COMMERCIAL)
+        ->setStreet("Rua da Quitanda")
+        ->setNumber("199")
+        ->setComplement("10º andar")
+        ->setDistrict("Centro")
+        ->setCity("Rio de Janeiro")
+        ->setState("RJ")
+        ->setZipCode("20091005")
+        ->setCountry(\Gateway\One\DataContract\Enum\CountryEnum::BRAZIL);
+
+    // Create new ApiClient object
+    $client = new Gateway\ApiClient();
+
+    // Make the call
+    $response = $client->createSale($request);
+
+    // Print the response
+    print "<pre>";
+    print json_encode(array('success' => $response->isSuccess(), 'data' => $response->getData()), JSON_PRETTY_PRINT);
+    print "</pre>";
+
+}
+catch (\Gateway\One\DataContract\Report\ApiError $error)
+{
+    // Print JSON
+    print "<pre>";
+    print json_encode($error, JSON_PRETTY_PRINT);
+    print "</pre>";
+}
+catch (Exception $ex)
+{
+    // Print JSON
+    print "<pre>";
+    print json_encode($ex, JSON_PRETTY_PRINT);
+    print "</pre>";
+}
